a33bed880a3f190ec2f1a2ad5f4ea9003090adce
UtaST <yutaxperia16@gmail.com>
2020-08-31 15:59:08 +0900

    ActiveHashに関するアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days_until_shipping
+  
   belongs_to :user
   has_one_attached :image
+
 end
