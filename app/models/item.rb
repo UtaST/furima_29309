1c921715461cde30e6eeaf76a817235de1b283ca
UtaST <yutaxperia16@gmail.com>
2020-08-31 19:17:09 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+  end
+
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :days_until_shipping_id
+  end
+
+  validates :price, numericality: { message: "half-width number"}
+  validates :price, numericality: { greater_than_or_equal_to:300, less_than: 10000000, message: "out of setting range" } 
 end
