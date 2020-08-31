9340c6955fd489fb9ceef51662a652cd7c4c9e76
UtaST <yutaxperia16@gmail.com>
2020-08-31 14:43:32 +0900

    index,new,createアクションをコントローラーに記述、ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category, :condition, :shipping_charge, :prefecture, :days_until_shipping, :price).merge(user_id: current_user.id)
+  end
 end
