ca81462f5ad35fa78ebe47488faebd90b6c2286f
UtaST <yutaxperia16@gmail.com>
2020-09-01 15:07:32 +0900

    Itemモデルのテスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.word}
+    explanation {Faker::Lorem.sentence}
+    category_id {Faker::Number.within(range: 1..10)}
+    condition_id {Faker::Number.within(range: 1..6)}
+    shipping_charge_id {Faker::Number.within(range: 1..2)}
+    prefecture_id {Faker::Number.within(range: 1..47)}
+    days_until_shipping_id {Faker::Number.within(range: 1..3)}
+    price {Faker::Number.within(range: 300..9999999)}
+
+    association :user
   end
 end
