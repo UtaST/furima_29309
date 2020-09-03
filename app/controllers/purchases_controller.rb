170de4fe679edb0f200d3b108ded96d39d1bfd1d
UtaST <yutaxperia16@gmail.com>
2020-09-03 18:19:18 +0900

    購入処理失敗時にエラーメッセージを表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,14 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase = PurchaseAddress.new
   end
 
   def create
     @purchase = PurchaseAddress.new(purchase_params)
-    if @purchase.save
+    if @purchase.valid?
+      @purchase.save
       redirect_to root_path
     else
       render :index
@@ -15,6 +16,10 @@ class PurchasesController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def purchase_params
     params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :buliding_name, :tell_number).merge(user_id: current_user.id, item_id: params[:item_id])
