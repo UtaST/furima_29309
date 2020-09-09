2507471e1f235eb03c8b8fc25e1fae74a1da9584
UtaST <yutaxperia16@gmail.com>
2020-09-09 16:50:30 +0900

    cardモデル、テーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
+  resources :cards, only: [:new, :create]
   resources :items do
     resources :purchases, only: [:index, :create]
   end
