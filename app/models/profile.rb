2a108b9f4902427c72ddcff8b42fae36b53696e3
UtaST <yutaxperia16@gmail.com>
2020-09-15 15:52:15 +0900

    optional: true設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,11 @@
 class Profile < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
   with_options presence: true do
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください" }
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください" }
-    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "は全角カタカナで入力してください" }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください" }
     validates :family_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "は全角カタカナで入力してください" }
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "は全角カタカナで入力してください" }
     validates :birthday
   end
 end
