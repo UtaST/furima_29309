00e1d4cc4f6a48639b6a7fc99bb06aa0d05df245
UtaST <yutaxperia16@gmail.com>
2020-09-16 17:47:23 +0900

    sns認証を用いたログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,15 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   
   def self.from_omniauth(auth)
-    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid),first_or_create
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
     user = User.where(email: auth.info.email).first_or_initialize(
       nickname: auth.info.name,
       email: auth.info.email
     )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
   end
 end
