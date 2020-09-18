d228cbcb2217536339be61eec93fd926605faff4
UtaST <yutaxperia16@gmail.com>
2020-09-18 15:32:46 +0900

    cardモデルの単体テストコード記述

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'クレジットカード情報登録' do
+    before do
+      @card = FactoryBot.build(:card)
+    end
+
+    it '全ての値が正しく入力されていれば登録できること' do
+      expect(@card).to be_valid
+    end
+
+    it 'カード情報正しくが入力されていないと登録できないこと' do
+      @card.token = nil
+      @card.valid?
+      expect(@card.errors.full_messages).to include 'カード情報を入力してください'
+    end
+  end
 end
